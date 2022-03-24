e76f01b13fb20a2bf6e6fa6f6273dcea68a00f1d
Akhtar <akhtar.husain@neudesic.com>
2022-03-24 16:16:38 +0530

    Add generics.

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,4 @@
+
 let helloWorld: string = "Hello World"
 
 interface User {
@@ -10,9 +11,10 @@ const user: User = {
     id: 0
 }  
 
+console.log(user)
 
 //  Type	        Predicate
-// ======          ===========
+// ======         ============
 // string	      typeof s === "string"
 // number         typeof n === "number"
 // boolean	      typeof b === "boolean"
@@ -27,4 +29,33 @@ const user: User = {
 // In a structural type system, if two objects have the same shape, they are considered to be of the same type.
 
 
-export {}
+
+// Since JavaScript supports classes and object - oriented programming, so does TypeScript. You can use an interface declaration with classes:
+
+
+// GENERICS
+// Generics provide variables to types.A common example is an array.An array without generics could contain anything.An array with generics can describe the values that the array contains.
+
+type StringArray = Array<string>;
+type NumberArray = Array<number>;
+type ObjectWithNameArray = Array<{ name: string }>;
+
+// You can declare your own types that use generics:
+
+interface Backpack<Type> {
+    add: (obj: Type) => void;
+    get: () => Type;
+}
+
+// This line is a shortcut to tell TypeScript there is a
+// constant called `backpack`, and to not worry about where it came from.
+declare const backpack: Backpack<string>;
+
+// object is a string, because we declared it above as the variable part of Backpack.
+const object = backpack.get();
+
+// Since the backpack variable is a string, you can't pass a number to the add function.
+backpack.add("23");
+
+console.log(backpack)
+
